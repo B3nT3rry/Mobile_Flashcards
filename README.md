f4a83a19476ce22cbaa77c1c74a697f1b39c095e
Ben Terry <benterry@Bens-MacBook-Pro.local>
2017-12-27 13:14:34 +1100

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Mobile Flashcards
+## React Nanodegree Project 3
+
+#### Supported platforms / devices?
+
+The app has only been tested on an iPhone 7 Plus running iOS 11 and a Nexus 5 running Android 6
+
+## Development usage
+
+```bash
+yarn          # install dependencies
+yarn start    # run app in development
+```
+
+## create-react-native-app
+
+This project was bootstrapped with [Create React Native App](https://github.com/react-community/create-react-native-app).  
+You can find more information on how to perform common tasks [here](https://github.com/react-community/create-react-native-app/blob/master/react-native-scripts/template/README.md).
+
+
---
+++ src/state/reducers/decks/initial-state.js
@@ -0,0 +1,29 @@
+export default {
+  React: {
+    title: 'React',
+    questions: [
+      {
+        question: 'What is React?',
+        answer: 'React is an open-source JavaScript library created by Facebook for building complex, interactive UIs in web and mobile applications.'
+      },
+      {
+        question: 'What is JSX?',
+        answer: 'JSX is a shorthand for JavaScript XML. This is a type of file used by React which utilizes the expressiveness of JavaScript along with HTML like template syntax. This makes the HTML file really easy to understand.'
+      },
+      {
+        question: 'What is Props?',
+        answer: 'Props are short hand for Properties in React. They are read-only components which must be kept pure i.e. immutable. They are always passed down from the parent to the child components through out the application. A child component can never send a prop back to the parent component. This help in maintaining the unidirectional data flow and are generally used to render the dynamically generated data.'
+      }
+    ]
+  },
+  JavaScript: {
+    title: 'JavaScript',
+    questions: [
+      {
+        question: 'What is negative infinity?',
+        answer:
+          'It’s a number in JavaScript, derived by dividing negative number by zero.'
+      }
+    ]
+  }
+}
